f146acde3ba8c99ef05e790c953f8d8f1c6ca595
Makar <kolesnykmakar0@gmail.com>
2025-11-05 08:21:16 +0200

    Add tasks

--- js.js
+++ js.js
@@ -1,15 +1,21 @@
 //string
 
-const str1 = "helLo wOrdl"
-const str2 = "hello wordl"
-console.log(str1);
+const userName = "bred";
 
-const str1UpperCase = str1.toUpperCase();
+const userNameTrim = userName.trim().toLowerCase();
+const fisrtLettarUpecerCase = userNameTrim.charAt(0).toUpperCase();
+const lastLettars = userNameTrim.slice(1);
+const finalyUserName = fisrtLettarUpecerCase + lastLettars;
+// console.log(finalyUserName);
 
-console.log(str1UpperCase);
-
-if (str1.toLowerCase() === str2) {
-  console.log("equal");
-} else {
-  console.log("not equal");
+while (true) {
+  const varibleUserName = prompt("Enter varible name");
+  const codeFirstLettar = varibleUserName.trim().charCodeAt(0);
+  if (codeFirstLettar >= 48 && codeFirstLettar <= 57) {
+    alert("please enter latter");
+    continue;
+  } else {
+    alert("all good");
+    break;
+  } 
 }
