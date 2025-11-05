0d262d5c90d927a17355d8d5ef063d655183930f
Makar <kolesnykmakar0@gmail.com>
2025-11-05 08:42:59 +0200

    Add task format

--- js.js
+++ js.js
@@ -1,21 +1,23 @@
 //string
 
-const userName = "bred";
-
-const userNameTrim = userName.trim().toLowerCase();
-const fisrtLettarUpecerCase = userNameTrim.charAt(0).toUpperCase();
-const lastLettars = userNameTrim.slice(1);
-const finalyUserName = fisrtLettarUpecerCase + lastLettars;
-// console.log(finalyUserName);
-
-while (true) {
-  const varibleUserName = prompt("Enter varible name");
-  const codeFirstLettar = varibleUserName.trim().charCodeAt(0);
-  if (codeFirstLettar >= 48 && codeFirstLettar <= 57) {
-    alert("please enter latter");
-    continue;
-  } else {
-    alert("all good");
+const fileName = prompt("enter file");
+const findLastPoint = fileName.lastIndexOf(".");
+const sliceAfterPoint = fileName.slice(findLastPoint+1).trim();
+switch (sliceAfterPoint) {
+  case "txt":
+  case "doc":
+    console.log("text");
     break;
-  } 
+  case "jpg":
+  case "png":
+    console.log("Image");
+    break;
+  case "html":
+  case "css":
+  case "js":
+    console.log("web");
+    break;
+    default:
+      console.log('unknown format');
+      break;
 }
