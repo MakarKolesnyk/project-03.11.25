19c77104d3427ebc8e9a33153086e72251c44b97
Makar <kolesnykmakar0@gmail.com>
2025-11-06 07:24:13 +0200

    Add sort & reverse

--- js.js
+++ js.js
@@ -1,18 +1,11 @@
 // reverse, sort
 
-const num = 359601;
-// const numToStr = String(num);
-// const arrStr = numToStr.split("");
-// const arrNums = arrStr.map((elem) => Number(elem));
-// const summa = arrNums.reduce((accumulator, elem) => accumulator + elem);
+const num = [5, 17, 1, 9, 10, 4, 2, 101];
+console.log(num.sort((a, b) => a - b));
+console.log(num);
 
-const result = String(num)
-  .split("")
-  .map((elem) => Number(elem))
-  .reduce((accumulator, elem) => accumulator + elem);
-console.log(result);
+// console.log(num.sort((a, b) => b - a));
+// console.log(num);
 
-const minDigit = Number (String(num).split('').sort().at(0))
-const maxDigit = Number (String(num).split('').sort().at(-1))
-console.log(maxDigit);
-console.log(minDigit);
+console.log(num.reverse());
+console.log(num);
